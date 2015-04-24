54694f7e439ed9ea2275c22fe1c1586b09c0933a
jdwd <justine.dewilde.pro@gmail>
2015-04-24 15:39:34 +0200

    Initialisation de Catapult : choix retenu > strucutre

---
+++ catapult.cpp
@@ -0,0 +1,25 @@
+typedef struct{
+    float hb;              //la hauteur de la butée
+    float lb;              //la longueur du bras Lb (en mètres)
+    float mb;              //la masse du bras mb (en kilos)
+    float beta;            //l'angle de la force de traction avec le bras β (en degrés)
+    float mc;              //la masse du contrepoids responsable de la force de traction mc (en kilos)
+    float mp;              //la masse du projectile mp (en kilos)
+    float lr;              //la longueur de la base Lr (en mètres)
+    float alpha;           //l'angle α (en degrés)
+}, CATAPULT;
+
+/* Fonctions relatice à la catapulte */
+
+
+/** A FAIRE
+ * FORCE DE TRACTION
+ * MOMENT DU BRAS
+ * MOMENT D'INERTIE DU BRAS
+ * ACCELERATION ANGULAIRE UNIFORME
+ * VELICITE
+ * PORTEE
+ * ENERGIE D'INPACTE
+ * Formule determinant si la constructione est VIABLE
+ * EQUIVALENCE Joule et TNT
+ */
--- main.cpp
+++ main.cpp
@@ -7,6 +7,24 @@
 
 #include <cstdlib>
 #include <iostream>
+/*
+ * Définition des constantes de pesanteur
+ */
+#define G_TERRE 9.81;       //m.s^-2
+#define G_LUNE 1.62;        //m.s^-2
+#define G_JUPITER 24.90;    //m.s^-2
+
+
+/*
+ * Génération d'une population de X individu
+ * Test de chaque individus
+ * Selection des meilleurs
+ * Création d'hybrides dans une nouvelle population
+ * Boucler tant que résultat n'évolue plus
+ * 
+ * OBJECTIF : éclater une cible à une distance d (energie cinétique)
+ */
+
 
 using namespace std;
 
