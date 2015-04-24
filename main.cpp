c5daaf19920fdf4aeb59c84b72e59f307a987a7a
maxoumime <maxime.bertheau@hotmail.fr>
2015-04-24 11:19:35 +0200

    Init Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: maxoumime
+ *
+ * Created on 24 avril 2015, 11:16
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "HEY HEY HEY" << endl;
+    return 0;
+}
+
