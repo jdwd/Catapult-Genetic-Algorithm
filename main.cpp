55eb83b3d80fcf6db97b178d8f0a37b2620907c5
jdwd <justine.dewilde.pro@gmail.com>
2015-05-18 18:51:01 +0200

    [JDWD]: commentaire indiquant démarche à suivre pour l'algorithme d'estimation d'évolution 		 Code à implémenter.

--- main.cpp
+++ main.cpp
@@ -33,38 +33,31 @@
 
 using namespace std;
 
-//TODO A VERIFIER
-bool isEnEvolution(int generationNbr, float moyennesGeneration[GENERATION_NBR_MINIMUM]){
- 
-    if(generationNbr <= GENERATION_NBR_MINIMUM)
-        return true;
-        
-    int meilleurScoreIndex = -1;
-    
-    for(int i = 0; i < GENERATION_NBR_MINIMUM; i++)
-        if(moyennesGeneration[i] > moyennesGeneration[meilleurScoreIndex] || meilleurScoreIndex == -1)
-            meilleurScoreIndex = i;
-    
-    float coefficient[GENERATION_NBR_MINIMUM -1];
-    int cpt = 0;
-    for(int i = 0; i < GENERATION_NBR_MINIMUM; i++){
-        if(i != meilleurScoreIndex){
-            coefficient[cpt] = 1 - (moyennesGeneration[i] / moyennesGeneration[meilleurScoreIndex]);
-            cpt++;
-        }
-    }
-    
-    return (coefficient[0] < ECART_VARIATION && coefficient[1] < ECART_VARIATION);
+
+// Méthode permettant de déterminer si les générations évoluent dans le bon sens 
+//et sont capable de produire une nouvelle génération plus évoluée
+bool isEvoluting(int generationNbr, float moyennesGeneration[3][GENERATION_NBR_MINIMUM]) {
+
+    // Determiner si la génération est à même de produire une génération plus performante
+    // revient à définir si :
+    // - La moyenne augmente : cela signifie que la génération entière évolue, mais pas forcément que la meilleure caatapute de chaque génération évolue donc l'indice n'est pas suffisant : NOTION d'ECART
+    // - La médiane augmente : autre indicateur de l'évolution d'une population, n'est pas affectée par les valeurs extremes, nous lui donnons donc tout autant de crédit : NOTION DE DISPERSION
+    // - La variance diminue : cet indicateur permet de vérifier si l'évolution des individus convergent vers un seul et même individut, les évolutions vont donc dans le même sens, à l'inverse l'évolution se disperse et n'arrvie pas à s'orienter
+     
+    //1ere étape : calcul du coefficient directeur
+    //2eme étape : calcul du pourcentage d'évolution
+    //3eme étape : calcul du coefficient d'évolution positif
+
 }
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    
+
     //Initialisation du random
     srand(time(NULL));
-    
+
     //Récupération du nombre d'individus à mettre en génération
     int nbrElementsPerPopulation;
     do {
@@ -72,38 +65,37 @@ int main(int argc, char** argv) {
         if (!(cin >> nbrElementsPerPopulation)) {
             cout << "Please enter numbers only." << endl;
             cin.clear();
-            cin.ignore(10000,'\n');
-        }    
-    }
-    while ((nbrElementsPerPopulation == 0) || (!Utils::isPairNumber(nbrElementsPerPopulation)));
-      
+            cin.ignore(10000, '\n');
+        }
+    } while ((nbrElementsPerPopulation == 0) || (!Utils::isPairNumber(nbrElementsPerPopulation)));
+
     //MOYENNE 0
     //VARIANCE 1
     //MEDIANE 2
     float indicateursGeneration[3][GENERATION_NBR_MINIMUM];
     int generationNbr = 0;
-    
+
     //Contient l'ancienne génération
     float** ancienneGeneration;
     //Contient la génération en cours, initialisation aléatoire de la 1ère population
     float** generation = Catapult::genererGeneration(nbrElementsPerPopulation, G_TERRE);
-      
+
     //Tant que nos éléments évoluent
-    do{
+    do {
         //Tri de la génération du plus haut score au plus faible
         Utils::sort(generation, nbrElementsPerPopulation);
 
         //Si la première génération a été traitée, purge des éléments
-        if(generationNbr > 0){
-            for(int i = 0; i < nbrElementsPerPopulation; i++){
+        if (generationNbr > 0) {
+            for (int i = 0; i < nbrElementsPerPopulation; i++) {
                 delete ancienneGeneration[i];
             }
             delete ancienneGeneration;
         }
-        
+
         //La génération a été calculée, on les passe donc en anciens (à reproduire)
         ancienneGeneration = generation;
-        
+
         //Initialisation de la nouvelle génération
         generation = new float *[nbrElementsPerPopulation];
 
@@ -111,63 +103,75 @@ int main(int argc, char** argv) {
         double scoreTotal = 0;
 
         //Pour tous les 2 éléments de la population
-        for(int cpt = 0; cpt < nbrElementsPerPopulation; cpt+=2){
-            
+        for (int cpt = 0; cpt < nbrElementsPerPopulation; cpt += 2) {
+
             //On ajoute le score de l'élément A et B
-            scoreTotal += ancienneGeneration[cpt][SCORE] + ancienneGeneration[cpt+1][SCORE];
-            
+            scoreTotal += ancienneGeneration[cpt][SCORE] + ancienneGeneration[cpt + 1][SCORE];
+
             //On croise les catapultes A et B pour en créer un enfant
-            generation[cpt] = Catapult::croiserCatapultes( ancienneGeneration[cpt], ancienneGeneration[cpt+1], G_TERRE );
+            generation[cpt] = Catapult::croiserCatapultes(ancienneGeneration[cpt], ancienneGeneration[cpt + 1], G_TERRE);
             //Mutation du nouveau né
             Catapult::mutation(generation[cpt]);
-            
+
             //Idem pour le frère
-            generation[cpt+1] = Catapult::croiserCatapultes( ancienneGeneration[cpt], ancienneGeneration[cpt+1], G_TERRE );
-            Catapult::mutation(generation[cpt+1]);
+            generation[cpt + 1] = Catapult::croiserCatapultes(ancienneGeneration[cpt], ancienneGeneration[cpt + 1], G_TERRE);
+            Catapult::mutation(generation[cpt + 1]);
         }
-        
+
         double scoreMoyenGeneration = scoreTotal / nbrElementsPerPopulation;
 
         //Mesure de la variance, necessite la moyenne des scores
         // 1/EFFECTIF*Epsilon((xn-MOYENNE)²)
         double varianceGeneration = 0;
-        for(int cpt = 0; cpt < nbrElementsPerPopulation; cpt++){
-            varianceGeneration += (pow(ancienneGeneration[cpt][SCORE]-scoreMoyenGeneration),2);
+        for (int cpt = 0; cpt < nbrElementsPerPopulation; cpt++) {
+            varianceGeneration += (pow((ancienneGeneration[cpt][SCORE] - scoreMoyenGeneration), 2));
         }
-        varianceGeneration = (varianceGeneration/nbrElementsPerPopulation);
-        
+        varianceGeneration = (varianceGeneration / nbrElementsPerPopulation);
+
         //Mesure de la médiane
         //Effectif de population forcément paire
-        int premierTerme = ancienneGeneration[(nbrElementsPerPopulation/2)];
-        int secondTerme = ancienneGeneration[(nbrElementsPerPopulation/2)+1];
+        float premierTerme = ancienneGeneration[(nbrElementsPerPopulation / 2)][SCORE];
+        float secondTerme = ancienneGeneration[(nbrElementsPerPopulation / 2) + 1][SCORE];
         //centre de l'intervalle formé par les deux termes
-        double medianeGeneration = premierTerme + ((premierTerme - secondTerme)/2);
-                
-        if(generationNbr <= 3){
-            indicateursGeneration[MOYENNE][generationNbr]  = scoreMoyenGeneration;
+        double medianeGeneration = premierTerme + ((premierTerme - secondTerme) / 2);
+
+        //Si les 3 premières générations n'ont pas encore été générées, on les ajoute "naturellement" au tableau        
+        if (generationNbr <= 3) {
+            indicateursGeneration[MOYENNE][generationNbr] = scoreMoyenGeneration;
             indicateursGeneration[VARIANCE][generationNbr] = varianceGeneration;
-            indicateursGeneration[MEDIANE][generationNbr]  = medianeGeneration;
-        }else {
+            indicateursGeneration[MEDIANE][generationNbr] = medianeGeneration;
+        } else {
             //Déplacement necessaire des résultats, car nous ne conservons que les 3 derniers
-            
-            indicateursGeneration[0] = indicateursGeneration[1];
-            indicateursGeneration[1] = indicateursGeneration[2];
-            indicateursGeneration[3] = scoreMoyenGeneration;
+            //On décale les scores sur la gauche, on cherche à avoir le score le plus récent en dernier
 
+            //MOYENNE
+            indicateursGeneration[MOYENNE][0] = indicateursGeneration[MOYENNE][1];
+            indicateursGeneration[MOYENNE][1] = indicateursGeneration[MOYENNE][2];
+            indicateursGeneration[MOYENNE][3] = scoreMoyenGeneration;
+
+            //VARIANCE
+            indicateursGeneration[VARIANCE][0] = indicateursGeneration[VARIANCE][1];
+            indicateursGeneration[VARIANCE][1] = indicateursGeneration[VARIANCE][2];
+            indicateursGeneration[VARIANCE][3] = varianceGeneration;
+
+            //MEDIANE
+            indicateursGeneration[MEDIANE][0] = indicateursGeneration[MEDIANE][1];
+            indicateursGeneration[MEDIANE][1] = indicateursGeneration[MEDIANE][2];
+            indicateursGeneration[MEDIANE][3] = medianeGeneration;
         }
-
 
         //if(generationNbr > 3)
         //    cout << moyennesGeneration[0] << ":" << moyennesGeneration[1] << ":" << moyennesGeneration[3] << endl;
-        
-        cout << "Génération n°" << generationNbr << ", score moyen:" << scoreMoyenGeneration << endl;
-        
-        generationNbr++;
+
+        cout << "Génération n°" << generationNbr << endl;
+        cout << "  -Moyenne: " << scoreMoyenGeneration << endl;
+        cout << "  -Variance: " << varianceGeneration << endl;
+        cout << "  -Médiane: " << medianeGeneration << endl;
 
         //Augmentation du nombre de générations ayant existé
         generationNbr++;
-        
-    }while( isEnEvolution(generationNbr, indicateursGeneration) );
+
+    } while (isEvoluting(generationNbr, indicateursGeneration));
     return 0;
 }
 
